87784b7ddb5d51556b370a355d433a5e48f6cd3c
Skeler667 <loloalilqwertykop@gmail.com>
2022-08-17 18:55:58 +0300

    3stepRefactoring

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,13 +1,10 @@
 import { readFileSync } from 'fs';
+import * as path from 'path';
+
 
 export default (filepath1, filepath2) => {
-
-const data = readFileSync(path, 'utf-8');
-const dataParseObject = JSON.parse(data)
-
-const data1 = readFileSync(filepath1, 'utf-8');
-const data2 = readFileSync(filepath2, 'utf-8');
-
-console.log(data1);
-console.log(data2);
+const pathToFile = path.resolve('__fixtures__', filepath1);
+const read = readFileSync(pathToFile, 'utf-8')
+const data = JSON.parse(read)
+console.log(data); 
 };
