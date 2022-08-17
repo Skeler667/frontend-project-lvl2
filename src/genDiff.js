479a15ef54bd1da966c5be2c333536a8a172e988
Skeler667 <loloalilqwertykop@gmail.com>
2022-08-17 17:04:42 +0300

    genDiffFix

--- src/genDiff.js
+++ src/genDiff.js
@@ -2,8 +2,8 @@ import { readFileSync } from 'fs';
 
 export default (filepath1, filepath2) => {
 
-// const data = readFileSync(path, 'utf-8');
-// const dataParseObject = JSON.parse(data)
+const data = readFileSync(path, 'utf-8');
+const dataParseObject = JSON.parse(data)
 
 const data1 = readFileSync(filepath1, 'utf-8');
 const data2 = readFileSync(filepath2, 'utf-8');
