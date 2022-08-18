2b617af1726f33130fe24469c4af2abdc1bd7b21
Skeler667 <loloalilqwertykop@gmail.com>
2022-08-18 12:20:12 +0300

    3step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,10 +6,11 @@ const program = new Command();
 
 program
   .version('6.6.6', '-V, --version', 'output the version number')
-  .argument('<filepath1> <filepath2>')
+  .argument('<filepath1>', 'first arg')
+  .argument('<filepath2>', 'second arg')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    return genDiff(filepath1, filepath2);
+    console.log(genDiff(filepath1, filepath2));
   });
 program.parse(process.argv);  
--- src/genDiff.js
+++ src/genDiff.js
@@ -5,7 +5,8 @@ import * as path from 'path';
 export default (filepath1, filepath2) => {
 const data1 = readFileSync(path.resolve('__fixtures__', filepath1), 'utf-8')
 const data2 = readFileSync(path.resolve('__fixtures__', filepath2), 'utf-8')
-console.log(data2);
+
 const parse1 = JSON.parse(data1)
 const parse2 = JSON.parse(data2)
+console.log(parse1, parse2);
 };
