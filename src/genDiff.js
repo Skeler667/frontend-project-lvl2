8ff20b684282274891a00edfec1e4ee05c32700b
Skeler667 <loloalilqwertykop@gmail.com>
2022-08-18 13:57:07 +0300

    deletSSH

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,13 +1,35 @@
 import { readFileSync } from 'fs';
 import * as path from 'path';
+import _ from 'lodash'
+
+const genDiff = (data1, data2) => {
+    const keys1 = Object.keys(data1);
+    const keys2 = Object.keys(data2);
+    const keys = _.union(keys1, keys2);
+  
+    const result = {};
+    for (const key of keys) {
+      if (!Object.hasOwn(data1, key)) {
+        result[key] = '-';
+      } else if (!Object.hasOwn(data2, key)) {
+        result[key] = '+';
+      } else if (data1[key] !== data2[key]) {
+        result[key] = '-';
+      } else {
+        result[key] = '';
+      }
+    }
+  
+    return result;
+  };
 
 
-export default (filepath1, filepath2) => {
+const parsing = (filepath1, filepath2) => {
 const data1 = readFileSync(path.resolve('__fixtures__', filepath1), 'utf-8')
 const data2 = readFileSync(path.resolve('__fixtures__', filepath2), 'utf-8')
 
-const parse1 = JSON.parse(data1)
-const parse2 = JSON.parse(data2)
-console.log(parse1);
-console.log(parse2);
+const parseData1 = JSON.parse(data1)
+const parseData2 = JSON.parse(data2)
 };
+
+export { genDiff, parsing };
