70ce311bddc5387eaa21c3c6f001cfdfa62485be
Skeler667 <loloalilqwertykop@gmail.com>
2022-08-19 18:36:24 +0300

    3Step

--- src/genDiff.js
+++ src/genDiff.js
@@ -7,9 +7,8 @@ const genDiff = (data1, data2) => {
     const keys = _.union(Object.keys(data1), Object.keys(data2));
     const result = {};
     for (const key of keys) {
-        console.log(data1[key])
         if (data1[key] == data2[key]) { 
-            result[key] = '';
+            result[key] = data1[key] + '';
         } 
         else if (data1.key !== data2[key]) {
             result[key] = '+';
