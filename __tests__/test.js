0534007aa1890c03b6921e43f1d0dd7629141e3b
Skeler667 <loloalilqwertykop@gmail.com>
2022-09-22 13:48:47 +0300

    final

--- __tests__/test.js
+++ __tests__/test.js
@@ -11,6 +11,7 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.resolve(__dirname, '../__fixtures__/', filename);
 const treeStylish = readFileSync(getFixturePath('stylish.txt'), 'utf-8');
 const treePlain = readFileSync(getFixturePath('plain.txt'), 'utf-8');
+const treeJson = readFileSync(getFixturePath('formatJson.txt'), 'utf-8');
 
 test(('stylish'), () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(treeStylish);
@@ -19,4 +20,8 @@ test(('stylish'), () => {
 test(('plain'), () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toBe(treePlain);
   expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'plain')).toEqual(treePlain);
+});
+test(('json'), () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toBe(treeJson);
+  expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'json')).toEqual(treeJson);
 });
--- src/builderTree.js
+++ src/builderTree.js
@@ -2,8 +2,8 @@ import _ from 'lodash';
 
 const buildTree = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
-  keys.sort();
-  return keys.map((key) => {
+  const sorted = _.sortBy(keys);
+  return sorted.map((key) => {
     if (!_.has(data1, key)) {
       return { type: 'added', key, value: data2[key] };
     }
