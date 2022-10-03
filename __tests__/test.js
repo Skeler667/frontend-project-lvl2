925f7be30489cd3faaabe9226295acb2eda1e288
Skeler667 <loloalilqwertykop@gmail.com>
2022-10-03 13:54:20 +0300

    add fix

--- __tests__/test.js
+++ __tests__/test.js
@@ -9,20 +9,17 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.resolve(__dirname, '../__fixtures__/', filename);
-const resultTreeStylish = readFileSync(getFixturePath('stylish.txt'), 'utf-8');
-const resultTreePlain = readFileSync(getFixturePath('plain.txt'), 'utf-8');
-const resultTreeJson = readFileSync(getFixturePath('formatJson.txt'), 'utf-8');
-const formaters = ['stylish', 'plain', 'json'];
+const resultStylish = readFileSync(getFixturePath('stylish.txt'), 'utf-8');
+const resultPlain = readFileSync(getFixturePath('plain.txt'), 'utf-8');
+const resultJson = readFileSync(getFixturePath('formatJson.txt'), 'utf-8');
 
 test(('stylish'), () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), formaters[0])).toBe(resultTreeStylish);
-  expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), formaters[0])).toEqual(resultTreeStylish);
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(resultStylish);
+  expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'stylish')).toEqual(resultStylish);
 });
 test(('plain'), () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toBe(resultTreePlain);
-  expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'plain')).toEqual(resultTreePlain);
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toBe(resultPlain);
 });
 test(('json'), () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toBe(resultTreeJson);
-  expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'json')).toEqual(resultTreeJson);
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toBe(resultJson);
 });
--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import parse from './parse.js';
 import buildDiff from './builderTree.js';
 import format from './formatters/index.js';
 
-const getExtension = (filepath) => path.extname(filepath).slice(1);
+const extractFormat = (filepath) => path.extname(filepath).slice(1);
+const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
+const getData = (filepath) => parse(readFileSync(filepath, 'utf-8'), extractFormat(filepath));
 
-const readFile = (filepath) => readFileSync(path.resolve(process.cwd(), filepath), 'utf-8');
-
-const genDiff = (filepath1, filepath2, type = 'stylish') => {
-  const data1 = parse(readFile(filepath1), getExtension(filepath1));
-  const data2 = parse(readFile(filepath2), getExtension(filepath2));
+const genDiff = (filepath1, filepath2, outputFormat = 'stylish') => {
+  const data1 = getData(getFullPath(filepath1));
+  const data2 = getData(getFullPath(filepath2));
   const tree = buildDiff(data1, data2);
-  return format(tree, type);
+  return format(tree, outputFormat);
 };
-
-genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json');
 
 export default genDiff;
